e64b184e67e7881cf20ed7d348e26eb82de8f0f6
matiasnga <matiasnga@icloud.com>
2022-08-13 07:30:58 -0300

    modify pro.xml

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,4 +4,5 @@ id INT IDENTITY PRIMARY KEY,
 name VARCHAR(200),
 role VARCHAR(200)
 );
-INSERT INTO  employee (id, name, role) VALUES (1, 'Matias Gallo', 'admin');
+INSERT INTO  employee (id, name, role) VALUES (1, 'Matias Gallo', 'dev');
+INSERT INTO  employee (id, name, role) VALUES (2, 'Federico Orellana', 'admin');
